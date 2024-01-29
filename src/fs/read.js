a2b359760553bb3265b55fc1b01b5b7125f8c2ef
b-kucherepa <bkuchr@gmail.com>
2024-01-30 01:58:21 +0400

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import { readFile } from 'fs';
+import { FS_DIR, FS_ERROR } from './fsconst';
+
 const read = async () => {
-    // Write your code here 
+    await readFile(FS_DIR + '/files/fileToRead.txt', 'utf8', (err, data) => {
+        if (err) {
+            throw new Error(FS_ERROR);
+        }
+        console.log(data);
+    });
 };
 
 await read();
