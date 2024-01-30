0ae4e2a00746efd18957dfb36632843ac19e8f9e
b-kucherepa <bkuchr@gmail.com>
2024-01-30 13:18:50 +0400

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+const PREFIX = '--';
 const parseArgs = () => {
-    // Write your code here 
+    let argString = '';
+    const argsArray = process.argv.slice(2);
+    for (let i = 0; i < argsArray.length-1; i++){
+        if (argsArray[i].startsWith(PREFIX)){
+            argString+=argsArray[i].slice(2) + ' is ' + argsArray[i + 1] + ', ';
+        }
+    }
+
+    console.log(argString.slice(0, -2));
 };
 
 parseArgs();
