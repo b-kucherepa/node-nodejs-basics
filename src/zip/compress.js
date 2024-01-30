f7de010070fbbfea4e0eb180780ff18991884e47
b-kucherepa <bkuchr@gmail.com>
2024-01-31 00:30:48 +0400

    feat: implement compress.js and decompress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,12 @@
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
+
 const compress = async () => {
-    // Write your code here 
+    const rs = createReadStream('src/zip/files/fileToCompress.txt');
+    const ws = createWriteStream('src/zip/files/archive.gz');
+    await pipeline(rs, createGzip(), ws);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,12 @@
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
+
 const decompress = async () => {
-    // Write your code here 
+    const rs = createReadStream('src/zip/files/archive.gz');
+    const ws = createWriteStream('src/zip/files/fileToCompress.txt');
+    await pipeline(rs, createGunzip(), ws);
 };
 
 await decompress();
