75ed11bf08f40f1513ea6b84bbb89207a5c9533f
b-kucherepa <bkuchr@gmail.com>
2024-01-31 02:04:17 +0400

    feat: implement transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
+class ReverseTransform extends Transform
+{
+    _reverseString (s) {
+        let chArr = s.split('');
+        chArr = chArr.reverse();
+        return chArr.join('');
+    }
+
+    _transform (chunk, encoding, callback) {
+        callback (null, this._reverseString(chunk.toString()) + '\n\n');
+    }
+}
+
 const transform = async () => {
-    // Write your code here 
+    const reverseStream = new ReverseTransform;
+    await pipeline(process.stdin, reverseStream, process.stdout);
 };
 
 await transform();
