daabfda1e1bcf60abc510c2c0e00811195467bd7
b-kucherepa <bkuchr@gmail.com>
2024-01-30 13:35:56 +0400

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+const PREFIX = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
-};
+    let argString = '';
+    const argsArray = Object.entries(process.env);
+    for (let i = 0; i < argsArray.length; i++){
+        if (argsArray[i][0].startsWith(PREFIX)){
+            argString+=argsArray[i][0] + '=' + argsArray[i][1] + ', ';
+        }
+    }
+
+    console.log(argString.slice(0, -2));};
 
 parseEnv();
