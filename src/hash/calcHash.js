8d0bc2cce2749572ea56b9cb3b305dab4a4ffec3
b-kucherepa <bkuchr@gmail.com>
2024-01-30 12:31:27 +0400

    feat: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { readFile } from 'fs';
+import { createHash } from 'crypto';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const HASH_DIR = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileData = readFile(HASH_DIR + '/files/fileToCalculateHashFor.txt', 'utf8', (err, data) => {});
+
+    const hashData = createHash('sha256');
+    hashData.update('fileData');
+    console.log(hashData.digest('hex'));
 };
 
 await calculateHash();
