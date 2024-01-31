fd22b8eb5065cb893f6a3e42ce9b8f84324a1185
b-kucherepa <bkuchr@gmail.com>
2024-01-31 15:21:16 +0400

    refactor: switch to fs/promises + try-catch approach

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,14 +1,14 @@
-import { cp } from 'fs';
+import { cp } from 'fs/promises';
 const PROJ_DIR = process.cwd();
 const FS_ERROR = 'FS operation failed';
 
 const copy = async () => {
-    console.log(PROJ_DIR + '/src/fs/files');
-    await cp(PROJ_DIR + '/src/fs/files', PROJ_DIR + '/src/fs/files_copy', { force: false, overwrite: false, errorOnExist: true, recursive: true }, (err) => {
-        if (err) {
-            throw new Error(FS_ERROR);
-        }
-    });
+    try {
+        await cp(PROJ_DIR + '/src/fs/files', PROJ_DIR + '/src/fs/files_copy', { force: false, overwrite: false, errorOnExist: true, recursive: true })
+    }
+    catch {
+        throw new Error(FS_ERROR);
+    }
 };
 
 await copy();
