755db820c15f4de3ba66cadc80e18a58a8169080
b-kucherepa <bkuchr@gmail.com>
2024-02-01 21:15:35 +0400

    feat: implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { fork } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const SCRIPT_PATH = __dirname + '/files/script.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProc = fork(SCRIPT_PATH, args, { stdio: 'pipe' });
+    process.stdin.pipe(childProc.stdin);
+    childProc.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([/* insert an argument array [arg1, arg2, ...] to test*/]);
